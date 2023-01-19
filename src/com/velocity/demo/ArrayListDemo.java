7e1bc1899c5c7f0c5bdc68414e88521f1b053091
Vaishnav <patilvaishnav222@gmail.com>
2023-01-19 18:16:58 +0530

    Array Class

--- src/com/velocity/demo/ArrayListDemo.java
+++ src/com/velocity/demo/ArrayListDemo.java
@@ -1,11 +1,18 @@
 package com.velocity.demo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ArrayListDemo {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello");
-
+		List<String> list = new ArrayList<String>();
+		list.add("Vaishnav");
+		list.add("Sanjay");
+		list.add("Patil");
+		
+		System.out.println(list);
 	}
 
 }
