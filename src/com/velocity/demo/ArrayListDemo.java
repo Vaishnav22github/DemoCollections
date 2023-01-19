cd02fe81ecf5d9278cc9515cb407795680448dff
Vaishnav <patilvaishnav222@gmail.com>
2023-01-19 18:00:45 +0530

    ArrayList Class Added

---
+++ src/com/velocity/demo/ArrayListDemo.java
@@ -0,0 +1,11 @@
+package com.velocity.demo;
+
+public class ArrayListDemo {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello");
+
+	}
+
+}
--- src/com/velocity/demo/HashMapDemo.java
+++ src/com/velocity/demo/HashMapDemo.java
@@ -1,10 +1,18 @@
 package com.velocity.demo;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class HashMapDemo {
 
 	public static void main(String[] args) {
 
-		System.out.println("Hello");
+		Map<Integer,String> map = new HashMap<Integer,String>();
+		map.put(101,"Vaishnav");
+		map.put(102,"Sanjay");
+		map.put(103,"Patil");
+		
+		System.out.println(map);
 	}
 
 }
